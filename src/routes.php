cd920462ba58a2a0c313620f525a45efff746857
felipe bastos <felipebastosweb@gmail.com>
2017-02-11 14:58:43 -0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,32 +1,63 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+use Phpba\Tree\Model\Specie;
+use Phpba\Tree\Model\Tree;
 
 $app->group('/specie', function() {
-    $this->get('/', function (){
+    $this->get('/', function (Request $request, Response $response, $args) {
         //TODO: /specie/index
+        $data = Specie::find('all');
+        //TODO: knew list error, refactore
+        return $response->write(json_encode($data->to_array()));
     });
-    $this->get('/{id:[0-9]+}', function (){
+    
+    $this->get('/{id:[0-9]+}', function () {
         //TODO: /specie/find
+        $data = Specie::find($request->getAttribute('id'));
+        return $response->write(json_encode($data->to_array()));
     });
-    $this->get('/{id:[0-9]+}/edit', function (){
+    
+    $this->get('/{id:[0-9]+}/edit', function () {
         //TODO: /specie/edit
+        $data = Specie::find($request->getAttribute('id'));
+        if(! $data) {
+            //TODO: report flash error message (Error to save Specie)
+        }
+        
+        return $response->write(json_encode($data->to_array()));
     });
-    $this->post('/', function (){
+    
+    $this->post('/', function (Request $request, Response $response, $args) {
         //TODO: /specie/index
+        $data = new Specie(['name' => $args['name'], 'name_popular' => $args['name_popular']]);
+        if(! $data->save()) {
+            //TODO: report flash error message (Error to save Specie)
+        }
+        return $response->write(json_encode($data->to_array()));
     });
-    $this->put('/{id:[0-9]+}', function (){
+    
+    $this->put('/{id:[0-9]+}', function (Request $request, Response $response, $args) {
         //TODO: /specie/update
+        $data = Specie::find($request->getAttribute('id'));
+        if(! $data) {
+            //TODO: report flash error message (Specie not found)
+        }
+        $data->update();
+        
+        return $response->write(json_encode($data->to_array()));
     });
-    $this->delete('/{id:[0-9]+}', function (){
+    
+    $this->delete('/{id:[0-9]+}', function (Request $request, Response $response, $args){
         //TODO: /specie/delete
+        $data = Specie::find($request->getAttribute('id'));
+        if(! $data->delete()) {
+            //TODO: report flash error message
+        }
+        return json_encode(TRUE);
     });
 });
 
