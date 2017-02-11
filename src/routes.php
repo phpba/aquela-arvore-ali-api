9fd059d8c05e059dc59e597221fab3c1b7dd5565
felipe bastos <felipebastosweb@gmail.com>
2017-02-11 12:17:21 -0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,28 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->group('/specie', function() {
+    $this->get('/', function (){
+        //TODO: /specie/index
+    });
+    $this->get('/{id:[0-9]+}', function (){
+        //TODO: /specie/find
+    });
+    $this->get('/{id:[0-9]+}/edit', function (){
+        //TODO: /specie/edit
+    });
+    $this->post('/', function (){
+        //TODO: /specie/index
+    });
+    $this->put('/{id:[0-9]+}', function (){
+        //TODO: /specie/update
+    });
+    $this->delete('/{id:[0-9]+}', function (){
+        //TODO: /specie/delete
+    });
+});
+
+$app->group('/tree', function() {
+
+});
